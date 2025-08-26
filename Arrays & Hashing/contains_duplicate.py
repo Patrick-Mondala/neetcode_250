ba37e677ecb071adba5d65fbfaea04627ed18686
Patrick Mondala <patrickmondala@gmail.com>
2025-08-25 21:30:36 -0700

    contains_duplicate update space time complexity notes

--- Arrays & Hashing/contains_duplicate.py
+++ Arrays & Hashing/contains_duplicate.py
@@ -3,8 +3,8 @@ Contains Duplicate
 Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 '''
 
-# Time Complexity: O(N) due to iteration through the nums array where N is the length of nums
-# Space Complexity: O(N) due to creating a set to store seen elements where N is the length of nums
+# Time Complexity: O(N) due to creating a set from the nums array where N is the length of nums
+# Space Complexity: O(N) due to creating a set which in the worst case can contain all elements of nums where N is the length of nums
 def hasDuplicate(nums: list[int]) -> bool:
     return len(set(nums)) != len(nums)
 
