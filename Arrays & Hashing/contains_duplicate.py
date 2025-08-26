2a3a87ad87b6e928128e8c716784e3a4665ffd3a
Patrick Mondala <patrickmondala@gmail.com>
2025-08-25 21:19:46 -0700

    contains_duplicate one line solution

--- Arrays & Hashing/contains_duplicate.py
+++ Arrays & Hashing/contains_duplicate.py
@@ -6,14 +6,7 @@ Given an integer array nums, return true if any value appears more than once in 
 # Time Complexity: O(N) due to iteration through the nums array where N is the length of nums
 # Space Complexity: O(N) due to creating a set to store seen elements where N is the length of nums
 def hasDuplicate(nums: list[int]) -> bool:
-    seen = set()
-
-    for num in nums:
-        if num in seen:
-            return True
-        seen.add(num)
-
-    return False
+    return len(set(nums)) != len(nums)
 
 # Test cases
 assert hasDuplicate([1, 2, 3, 3]) == True
