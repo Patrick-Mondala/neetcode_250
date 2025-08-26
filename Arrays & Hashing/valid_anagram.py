39f5cf741cc40a861bbd807429023c90069ad38a
Patrick Mondala <patrickmondala@gmail.com>
2025-08-25 21:30:09 -0700

    valid_anagram array freqmap sln

--- Arrays & Hashing/valid_anagram.py
+++ Arrays & Hashing/valid_anagram.py
@@ -8,25 +8,16 @@ An anagram is a string that contains the exact same characters as another string
 # Time Complexity: O(N + M) where N is the length of the strings s and M is the length of the string t
 # Space Complexity: O(1) since the frequency map will at most contain 26 key-value pairs for each letter in the English alphabet
 def isAnagram(s: str, t: str) -> bool:
-    freqMap = {}
+    freq = [0] * 26  # Frequency map for each letter in the English alphabet
 
-    for char in s:
-        if char not in freqMap:
-            freqMap[char] = 0
-        freqMap[char] += 1
+    if len(s) != len(t):
+        return False
     
-    for char in t:
-        if char not in freqMap:
-            return False
-        if freqMap[char] == 0:
-            return False
-        freqMap[char] -= 1
-    
-    for char, count in freqMap.items():
-        if count != 0:
-            return False
-    
-    return True
+    for i in range(len(s)):
+        freq[ord(s[i]) - ord('a')] += 1
+        freq[ord(t[i]) - ord('a')] -= 1
+
+    return all(count == 0 for count in freq)
 
 # Test cases
 assert isAnagram("racecar", "carrace") == True
