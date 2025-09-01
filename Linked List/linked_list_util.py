958509cfe5ab2624d7b96f46d23c9ee9e9107b6c
Patrick Mondala <patrickmondala@gmail.com>
2025-08-31 21:47:53 -0700

    add linked list util to convert lists to linked lists and linked lists to lists

---
+++ Linked List/linked_list_util.py
@@ -0,0 +1,28 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def list_to_linked_list(arr: list) -> ListNode:
+    prev = None
+    head = None
+
+    for val in arr:
+        cur = ListNode(val)
+        if prev:
+            prev.next = cur
+        else:
+            head = cur
+        prev = cur
+
+    return head
+
+def linked_list_to_list(head: ListNode | None = None) -> list:
+    res = []
+    cur = head
+
+    while cur:
+        res.append(cur.val)
+        cur = cur.next
+
+    return res
